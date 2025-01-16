4b13f40f3b5be13660ed20aef612a6f3abd12e94
Marcelo Cambero <camberotje@gmail.com>
2025-01-16 19:01:53 +0000

    domain generator MEJORADO

--- src/app.js
+++ src/app.js
@@ -8,16 +8,19 @@ import "./assets/img/4geeks.ico";
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
+let color = ["red", "blue"];
 
 let container = document.getElementById("domain");
 let domainList = "";
 
 for (let p = 0; p < pronoun.length; p++) {
-  for (let a = 0; a < adj.length; a++) {
-    for (let n = 0; n < noun.length; n++) {
-      //
-      let domainName = pronoun[p] + adj[a] + noun[n];
-      domainList += domainName + "<br>";
+  for (let c = 0; c < color.length; c++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        let domainName = pronoun[p] + color[c] + adj[a] + noun[n];
+
+        domainList += domainName + ".com" + "<br>";
+      }
     }
   }
 }
